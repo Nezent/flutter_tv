e7ccc67e7f43083ad753fb016d034f763eef357c
Sirajum Munir <nezentbd@gmail.com>
2024-05-20 09:51:49 +0600

    tv control

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -12,19 +12,25 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  late FocusScopeNode _screenFocusNode;
+  late FocusNode _focusNode;
+  // final GlobalKey _key = GlobalKey();
 
   @override
   void initState() {
-    _screenFocusNode = FocusScopeNode(debugLabel: "Screen_focus_node");
+    _focusNode = FocusNode(debugLabel: "Movie_list_focus_node");
+    _focusNode.addListener(_onFocusChanged);
     super.initState();
   }
 
   @override
   void dispose() {
-    _screenFocusNode.dispose();
+    _focusNode.removeListener(_onFocusChanged);
+    _focusNode.dispose();
     super.dispose();
   }
+
+  int selectedIndex = -1;
+  int selectedIndex2 = -1;
 
   @override
   Widget build(BuildContext context) {
@@ -33,64 +39,141 @@ class _HomeScreenState extends State<HomeScreen> {
         title: const Text('Flutter TV'),
       ),
       body: SingleChildScrollView(
-        child: FocusScope(
-          autofocus: true,
-          node: _screenFocusNode,
-          child: sampleVideoGrid(),
-        ),
+        child: sampleVideoGrid(),
       ),
     );
   }
 
   Widget sampleVideoGrid() {
-    return SingleChildScrollView(
-      child: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Movies"),
-            SizedBox(
-              height: 16,
-            ),
-            SizedBox(
-              height: 100,
-              child: ListView(
-                scrollDirection: Axis.horizontal,
-                shrinkWrap: true,
-                children: images
-                    .map(
-                      (url) => VideoList(
-                        url: url,
-                        autoFocus: images.indexOf(url) == 0,
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Movies"),
+          SizedBox(
+            height: 16,
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              itemCount: images.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return Focus(
+                  onFocusChange: (value) {
+                    setState(() {
+                      selectedIndex = index;
+                    });
+                  },
+                  child: InkWell(
+                    onFocusChange: (value) {
+                      setState(() {
+                        selectedIndex = value ? index : -1;
+                      });
+                    },
+                    radius: 0.0,
+                    splashColor: Colors.transparent,
+                    focusColor: Colors.transparent,
+                    hoverColor: Colors.transparent,
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (context) => VideoPlayerScreen(
+                            url: links[index],
+                          ),
+                        ),
+                      );
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: AnimatedContainer(
+                        // key: _key,
+                        duration: const Duration(milliseconds: 200),
+                        curve: Curves.easeInOut,
+                        height: 100,
+                        width: (index == selectedIndex) ? 200 : 150,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(8),
+                          image: DecorationImage(
+                              image: NetworkImage(
+                                images[index],
+                              ),
+                              fit: BoxFit.cover),
+                          border: (index == selectedIndex)
+                              ? Border.all(color: Colors.blue[900]!, width: 2)
+                              : null,
+                        ),
                       ),
-                    )
-                    .toList(),
-              ),
+                    ),
+                  ),
+                );
+              },
             ),
-            SizedBox(
-              height: 30,
+          ),
+          SizedBox(
+            height: 30,
+          ),
+          Text("TV Series"),
+          SizedBox(
+            height: 16,
+          ),
+          SizedBox(
+            height: 100,
+            child: ListView.builder(
+              itemCount: images.length,
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (BuildContext context, int index) {
+                return InkWell(
+                  onFocusChange: (value) {
+                    setState(() {
+                      selectedIndex2 = value ? index : -1;
+                    });
+                  },
+                  radius: 0.0,
+                  splashColor: Colors.transparent,
+                  focusColor: Colors.transparent,
+                  hoverColor: Colors.transparent,
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      CupertinoPageRoute(
+                        builder: (context) => VideoPlayerScreen(
+                          url: links[index],
+                        ),
+                      ),
+                    );
+                  },
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: AnimatedContainer(
+                      // key: _key,
+                      duration: const Duration(milliseconds: 200),
+                      curve: Curves.easeInOut,
+                      height: 100,
+                      width: (index == selectedIndex2) ? 200 : 150,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        image: DecorationImage(
+                            image: NetworkImage(
+                              images[index],
+                            ),
+                            fit: BoxFit.cover),
+                        border: (index == selectedIndex2)
+                            ? Border.all(color: Colors.blue[900]!, width: 2)
+                            : null,
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-            Text("TV Series"),
-            SizedBox(
-              height: 16,
-            ),
-            // SizedBox(
-            //   height: 100,
-            //   child: ListView(
-            //     scrollDirection: Axis.horizontal,
-            //     shrinkWrap: true,
-            //     children: images
-            //         .map((url) => VideoList(
-            //               url: url,
-            //               autoFocus: images.indexOf(url) == 0,
-            //             ))
-            //         .toList(),
-            //   ),
-            // ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
+
+  void _onFocusChanged() => setState(() {});
 }
--- lib/list_view.dart
+++ lib/list_view.dart
@@ -1,80 +1,90 @@
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_tv/raw_data.dart';
-import 'package:flutter_tv/video_player_screen.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter_tv/raw_data.dart';
+// import 'package:flutter_tv/video_player_screen.dart';
 
-class VideoList extends StatefulWidget {
-  final String url;
-  final bool autoFocus;
-  const VideoList({super.key, required this.url, required this.autoFocus});
+// class VideoList extends StatefulWidget {
+//   final String url;
+//   final bool autoFocus;
+//   const VideoList({super.key, required this.url, required this.autoFocus});
 
-  @override
-  State<VideoList> createState() => _VideoListState();
-}
+//   @override
+//   State<VideoList> createState() => _VideoListState();
+// }
 
-class _VideoListState extends State<VideoList> {
-  late FocusNode _focusNode;
-  final GlobalKey _key = GlobalKey();
+// class _VideoListState extends State<VideoList> {
+//   late FocusNode _focusNode;
+//   final GlobalKey _key = GlobalKey();
 
-  @override
-  void initState() {
-    _focusNode = FocusNode(debugLabel: "Movie_list_focus_node");
-    _focusNode.addListener(_onFocusChanged);
-    super.initState();
-  }
+//   @override
+//   void initState() {
+//     _focusNode = FocusNode(debugLabel: "Movie_list_focus_node");
+//     _focusNode.addListener(_onFocusChanged);
+//     super.initState();
+//   }
 
-  @override
-  void dispose() {
-    _focusNode.removeListener(_onFocusChanged);
-    _focusNode.dispose();
-    super.dispose();
-  }
+//   @override
+//   void dispose() {
+//     _focusNode.removeListener(_onFocusChanged);
+//     _focusNode.dispose();
+//     super.dispose();
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return InkWell(
-      radius: 0.0,
-      splashColor: Colors.transparent,
-      focusColor: Colors.transparent,
-      hoverColor: Colors.transparent,
-      onTap: () {
-        print("hello");
-        Navigator.push(
-          context,
-          CupertinoPageRoute(
-            builder: (context) => VideoPlayerScreen(
-              url: links[images.indexOf(widget.url)],
-            ),
-          ),
-        );
-      },
-      child: Focus(
-        focusNode: _focusNode,
-        autofocus: widget.autoFocus,
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: AnimatedContainer(
-            key: _key,
-            duration: const Duration(milliseconds: 200),
-            curve: Curves.easeInOut,
-            height: 100,
-            width: _focusNode.hasFocus ? 200 : 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              image: DecorationImage(
-                  image: NetworkImage(
-                    widget.url,
-                  ),
-                  fit: BoxFit.cover),
-              border: _focusNode.hasFocus
-                  ? Border.all(color: Colors.blue[900]!, width: 2)
-                  : null,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
+//   int selectedIndex = -1;
 
-  void _onFocusChanged() => setState(() {});
-}
+//   @override
+//   Widget build(BuildContext context) {
+//     return InkWell(
+//       onFocusChange: (value) {
+//         setState(() {});
+//       },
+//       radius: 0.0,
+//       splashColor: Colors.transparent,
+//       focusColor: Colors.transparent,
+//       hoverColor: Colors.transparent,
+//       onTap: () {
+//         Navigator.push(
+//           context,
+//           CupertinoPageRoute(
+//             builder: (context) => VideoPlayerScreen(
+//               url: links[images.indexOf(widget.url)],
+//             ),
+//           ),
+//         );
+//       },
+//       child: Focus(
+//         onFocusChange: (value) {
+//           setState(() {
+//             selectedIndex = images.indexOf(widget.url);
+//             print(selectedIndex);
+//           });
+//         },
+//         focusNode: _focusNode,
+//         autofocus: widget.autoFocus,
+//         child: Padding(
+//           padding: const EdgeInsets.all(8.0),
+//           child: AnimatedContainer(
+//             key: _key,
+//             duration: const Duration(milliseconds: 200),
+//             curve: Curves.easeInOut,
+//             height: 100,
+//             width: _focusNode.hasFocus ? 200 : 150,
+//             decoration: BoxDecoration(
+//               borderRadius: BorderRadius.circular(8),
+//               image: DecorationImage(
+//                   image: NetworkImage(
+//                     widget.url,
+//                   ),
+//                   fit: BoxFit.cover),
+//               border: _focusNode.hasFocus
+//                   ? Border.all(color: Colors.blue[900]!, width: 2)
+//                   : null,
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+
+//   void _onFocusChanged() => setState(() {});
+// }
